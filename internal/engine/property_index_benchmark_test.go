9b5a5febeca86dfef7807fe09694af94c1c9f75f
ChanYub Park <mrchypark@gmail.com>
2026-09-05 13:53:08 +0900

    engine: skip unrelated property indexes on updates (#139)

--- internal/engine/property_index_benchmark_test.go
+++ internal/engine/property_index_benchmark_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strconv"
 	"testing"
 )
 
@@ -31,6 +32,59 @@ func BenchmarkCreateNodePropertyIndexContext(b *testing.B) {
 					b.Fatal(err)
 				}
 				if err := db.DropNodePropertyIndex("Item", "key"); err != nil {
+					b.Fatal(err)
+				}
+			}
+			b.StopTimer()
+			if err := db.Close(); err != nil {
+				b.Fatal(err)
+			}
+		})
+	}
+}
+
+func BenchmarkPropertyIndexMutation(b *testing.B) {
+	for _, test := range []struct {
+		name             string
+		unrelatedIndexes int
+		propertyCount    int
+		labelCount       int
+	}{
+		{name: "unrelated_indexes", unrelatedIndexes: 512, propertyCount: 1},
+		{name: "wide_record", propertyCount: 512},
+		{name: "many_labels_few_properties", unrelatedIndexes: 16, propertyCount: 1, labelCount: 32},
+	} {
+		b.Run(test.name, func(b *testing.B) {
+			db, err := Open(filepath.Join(b.TempDir(), "db"), OpenOptions{Create: true, WALCheckpointThresholdBytes: ^uint64(0)})
+			if err != nil {
+				b.Fatal(err)
+			}
+			properties := map[string]any{"key": int64(0)}
+			for i := 1; i < test.propertyCount; i++ {
+				properties["property"+strconv.Itoa(i)] = int64(i)
+			}
+			labels := []string{"Item"}
+			for i := 1; i < test.labelCount; i++ {
+				labels = append(labels, "Label"+strconv.Itoa(i))
+			}
+			if err := db.Update(func(tx *Tx) error {
+				_, err := tx.CreateNode(CreateNodeOptions{Labels: labels, Properties: properties})
+				return err
+			}); err != nil {
+				b.Fatal(err)
+			}
+			if err := db.CreateNodePropertyIndex("Item", "key"); err != nil {
+				b.Fatal(err)
+			}
+			for i := 0; i < test.unrelatedIndexes; i++ {
+				if err := db.CreateNodePropertyIndex("Other"+strconv.Itoa(i), "key"); err != nil {
+					b.Fatal(err)
+				}
+			}
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if err := db.Update(func(tx *Tx) error { return tx.SetProperty(1, "key", int64(i)) }); err != nil {
 					b.Fatal(err)
 				}
 			}
