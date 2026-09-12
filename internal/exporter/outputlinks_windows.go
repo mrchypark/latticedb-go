bcc2a3b87089bf1a3b0b4267ab4e792d6640c07d
Chanyub.Park <mrchypark@gmail.com>
2026-09-12 14:41:14 +0900

    Allow Windows metadata checks during export replacement

--- internal/exporter/outputlinks_windows.go
+++ internal/exporter/outputlinks_windows.go
@@ -3,19 +3,10 @@
 package exporter
 
 import (
+	"github.com/mrchypark/latticedb-go/internal/store"
 	"os"
-	"syscall"
 )
 
 func exportOutputHasMultipleLinks(path string, _ os.FileInfo) (bool, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return false, err
-	}
-	defer file.Close()
-	var info syscall.ByHandleFileInformation
-	if err := syscall.GetFileInformationByHandle(syscall.Handle(file.Fd()), &info); err != nil {
-		return false, err
-	}
-	return info.NumberOfLinks > 1, nil
+	return store.FileHasMultipleLinks(path)
 }
